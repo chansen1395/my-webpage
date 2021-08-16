89481638ace79aea3cb2da3005bc2a6c36776f2d
Connor Hansen <chansen13@georgefox.edu>
2021-08-16 10:58:41 -0700

    add a list generated by the user. Empties the list on re-submit.

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+
+ 
+
+  $("#form1").submit(function (event) {
+    event.preventDefault();
+
+    // Empties the div for a new list
+    $("#myList").empty();
+
+    // let list = document.getElementById("myList");
+    let listElement = document.createElement("ul");
+    document.getElementById("myList").appendChild(listElement);
+    
+
+    const names = $("#name").val();
+    const places = $("#place").val();
+    const foods = $("#food").val();
+    const addresses = $("#address").val();
+
+    const array1 = [];
+    const array2 = [];
+
+    array1.push(names, places, foods, addresses);
+    array2.push(array1[1], array1[0], array1[2]);
+
+    array1.forEach((item)=>{
+      let li = document.createElement("li");
+      li.innerText = item;
+      listElement.appendChild(li);
+  });
+
+
+  // Sets previous array length to 0, effectively clearing the contents.
+  // array1.length = 0;
+
+  //   array2.forEach((item)=>{
+  //     let li = document.createElement("li");
+  //     li.innerText = item;
+  //     list.appendChild(li);
+  // });
+
+
+
+  // let listElement = document.createElement("ul");
+  // document.getElementById("myList").appendChild(listElement);
+
+  //   array2.forEach(function (item) {
+  //     let li = document.createElement("li");
+  //     listElement.appendChild(li);
+  //     li.innerHTML += item;
+  // });
+
+
+});
+});
+
